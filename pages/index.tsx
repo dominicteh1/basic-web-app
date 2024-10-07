2d52984d9042ed5c9e1139d9c20ecde7ec4f77df
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2024-10-07 11:25:46 -0400

    removed example queries as it caused issues with deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,6 @@ export default function Home() {
     <div>
       <h1>Welcome!!</h1>
       <p>Please enter your query in the box below:</p>
-      <p>Example queries: "What is the capital of France?"</p>
-      <p>Example queries: "Who is Shakespeare?"</p>
-      <p>Example queries: "What is your Andrew ID?"</p>
       <input
         type="text"
         value={query}
