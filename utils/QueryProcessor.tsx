a6fec9f6a228191580c25d207a75b0a56e503548
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2024-10-10 10:26:18 -0400

    add stuff

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -75,6 +75,11 @@ export default function QueryProcessor(query: string): string {
         }
       }
     }
-
+    else if (query.toLowerCase().includes("to the power of")) {
+      const numbers = query.match(/\d+/g)?.map(Number);
+      if (numbers && numbers.length === 2) {
+        return Math.pow(numbers[0], numbers[1]).toString();
+      }
+    }
   return "";
 }
